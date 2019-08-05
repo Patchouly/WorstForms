fed8e1774e4a2503ab0f830ce1e705f827d0aa65
Pedro Cardoso <pedropaulo.cardoso@banese.com.br>
2019-08-05 08:24:39 -0300

    Paginas randomizadas

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,16 @@
 module.exports.index = function(application, req, res) {
     res.render("index");
 };
+
+module.exports.form1 = function(application, req, res) {
+    res.render("form1");
+};
+module.exports.form2 = function(application, req, res) {
+    res.render("form2");
+};
+module.exports.form3 = function(application, req, res) {
+    res.render("form3");
+};
+module.exports.form4 = function(application, req, res) {
+    res.render("form4");
+};
--- app/routes/homeRT.js
+++ app/routes/homeRT.js
@@ -3,7 +3,23 @@ module.exports = function(application) {
 		application.app.controllers.home.index(application, req, res);
 	});
 
-	application.get('/new', function(req, res) {
-		application.app.controllers.home.index(application, req, res);
+	application.post('/new', function(req, res) {
+		var randon = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+		switch(randon) {
+			case 1:
+				application.app.controllers.home.form1(application, req, res);
+				break;
+			case 2:
+				application.app.controllers.home.form2(application, req, res);
+				break;
+			case 3:
+				application.app.controllers.home.form3(application, req, res);
+				break;
+			case 4:
+				application.app.controllers.home.form4(application, req, res);
+				break;	
+			default:
+				application.app.controllers.home.index(application, req, res);
+		  }
 	});
 };
