cbb3a92042482b87cefeea40aae2edd9edb2d8c0
Patchouly <patchoulypeps@gmail.com>
2019-08-12 23:37:38 -0300

    Ajustes do submit e layout

--- app/views/assets/js/phoneForm.js
+++ app/views/assets/js/phoneForm.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('select').formSelect();
+    M.AutoInit();
 
     $('#logout_btn').click(function(){
         window.location.href = '/logout';
@@ -10,15 +10,30 @@ $(document).ready(function() {
         $('.digit').each(function( index ) {
             number += $(this).children("option:selected").val();
         });
-        $('.phoneNumber').val(formatPhone(number));
+        $('.hiddenPhone').val(formatPhone(number));
     });
 
     $('.form2range').on('input', function(){
         var number = $(this).val();
         $('#indexPhoneNumber').val(formatPhone(number));
+        $('.hiddenPhone').val(formatPhone(number));
     });
+    addForm3Values();
 });
 
 function formatPhone(number) {
     return number.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+}
+
+function addForm3Values() {
+    console.log("adicionando options");
+    for (var i = 0; i < 99; i++) {
+        $(".region").append(new Option("i", "i"));
+    }
+    for (var i = 0; i < 99999; i++) {
+        $(".5digits").append(new Option("i", "i"));
+    }
+    for (var i = 0; i < 9999; i++) {
+        $(".4digits").append(new Option("i", "i"));
+    }
 }
